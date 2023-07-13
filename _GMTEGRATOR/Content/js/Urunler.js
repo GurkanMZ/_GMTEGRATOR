cba7ad6e72b7346ee3cf0fc11b32650e194adde0
[GurkanMZ] <[gurkan.mizrak@hotmail.com]>
2023-07-13 15:08:14 +0300

    Urunlistesi Gelmedi.

---
+++ _GMTEGRATOR/Content/js/Urunler.js
@@ -0,0 +1,40 @@
+﻿
+
+$(document).ready(function () {
+
+    var tableData = [
+        { id: 1, name: "Billy Bob", age: "12", gender: "male", height: 1, col: "red", dob: "", cheese: 1 },
+        { id: 2, name: "Mary May", age: "1", gender: "female", height: 2, col: "blue", dob: "14/05/1982", cheese: true },
+    ];
+    var tblUrunler = new Tabulator("#tblUrunler", {
+        layout: "fitDataTable",
+        pagination: "local",
+        paginationSize: 15,
+        columns: [{ field: "STOK_KODU", title: "STOK_KODU" }],
+    });
+
+
+    $.ajax({
+        type: "GET",
+        url: "/Home/TumUrunlerList",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (Urunlistesi) {
+            var dataaa = Urunlistesi;
+            tblUrunler.on("tableBuilt", function () {
+                tblUrunler.setData(dataaa);
+            });
+        },
+        failure: function (response) {
+            alert("failure");
+        },
+        error: function (response) {
+            alert("errorrr");
+        }
+    });
+
+
+
+
+
+});
---
+++ _GMTEGRATOR/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Services;
+using _GMTEGRATOR.Models;
+
+namespace _GMTEGRATOR.Controllers
+{
+    public class HomeController : Controller
+    {
+        deartech_3Entities context = new deartech_3Entities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult TumUrunlerList()
+        {
+            var Urunlistesi = context.GM_TBLSTSABIT.ToList();
+            return Json(Urunlistesi, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+
+
+
+    }
+}
