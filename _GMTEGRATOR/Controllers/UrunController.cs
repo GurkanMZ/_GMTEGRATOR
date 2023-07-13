9dcbff3ec1090fb4c7684564dc53ba9eed3cb119
[GurkanMZ] <[gurkan.mizrak@hotmail.com]>
2023-07-13 11:43:17 +0300

    Tekli Ürün Ekleme İşlemi Gerçelşetirildi.

--- _GMTEGRATOR/Controllers/UrunController.cs
+++ _GMTEGRATOR/Controllers/UrunController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _GMTEGRATOR.Models;
 
 namespace _GMTEGRATOR.Controllers
 {
     public class UrunController : Controller
     {
+        deartech_3Entities context = new deartech_3Entities();
         // GET: Urun
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult UrunKayitMethod(GM_TBLSTSABIT stsabit)
+        {            
+            context.GM_TBLSTSABIT.Add(stsabit);
+            context.SaveChanges();
+            return RedirectToAction("Index","Urun");
+        }
+
+
     }
 }
